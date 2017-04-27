6fb481640da3736ffc391c67c00d14fbeaefd138
artempy <artemon5@gmail.com>
2017-04-27 16:19:43 +0300

    Refactoring urls. Alter category handler. Add class-based view for the category and add paginate for the category

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -5,7 +5,8 @@ from apps.posts import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^(?P<category_slug>[\-\w]+)/$', views.show_cat, name='category'),
+    url(r'^(?P<category_slug>[\-\w]+)(/page/(?P<page>[1-9][0-9]?))?/$',
+        views.ShowCat.as_view(paginate_by=3), name='category'),
     url(r'^(?P<category_slug>[\-\w]+)/(?P<article_slug>[\-\w]+)\.html$',
         views.show_article, name='article'),
 ]
