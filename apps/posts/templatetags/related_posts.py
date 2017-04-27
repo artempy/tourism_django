d161d7246c627eec577cf79fa732b4002ffdbcbb
artempy <artemon5@gmail.com>
2017-04-27 16:14:47 +0300

    Bug fix forming similar articles for the article

--- apps/posts/templatetags/related_posts.py
+++ apps/posts/templatetags/related_posts.py
@@ -1,13 +1,18 @@
 from django import template
-from apps.posts.models import Article
 from tagging.models import TaggedItem
+from apps.posts.models import Article
 
 
 register = template.Library()
 
 
 @register.inclusion_tag('templatetags/related_posts.html')
-def related_posts(tags):
+def related_posts(tags, current_id_article):
     """Get similar articles by any tags from this article"""
-    posts = TaggedItem.objects.get_union_by_model(Article, tags)
+    posts = TaggedItem.objects.get_union_by_model(
+        Article, tags
+    ).exclude(
+        pk=current_id_article
+    )[:4]
+
     return {'related_posts': posts}
