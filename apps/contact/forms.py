b6843b09ff7d78f5791c81e1509f014e762354d2
artempy <artemon5@gmail.com>
2017-05-02 18:25:44 +0300

    Create form for contact with admin

--- apps/contact/forms.py
+++ apps/contact/forms.py
@@ -4,15 +4,15 @@ from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
 
-class ContactForm(forms.ModelForm):
-    name = forms.CharField(max_length=64, label='Ваше имя',
-                           help_text='Введите ваше имя')
-    email = forms.EmailField(label='Email', help_text='Введите ваш Email')
-    message = forms.CharField(widget=forms.Textarea, label='Текст')
-    captcha = ReCaptchaField(widget=ReCaptchaWidget())
-    #datetime.now().strftime("%d.%m.%y в %H:%M")
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=45, label='Ваше имя')
+    email = forms.EmailField(label='Email')
+    subject = forms.CharField(max_length=40, label='Тема')
+    message = forms.CharField(widget=forms.Textarea, label='Сообщение')
+    captcha = ReCaptchaField(widget=ReCaptchaWidget(), label='Вы робот?')
 
     def clean_message(self):
+        """Check length of message"""
         message = self.cleaned_data['message']
         num_words = len(message.split())
         if num_words < 4:
