80c99ffeea47df2bce5f5854bfe59f9fd3224438
artempy <artemon5@gmail.com>
2017-05-02 18:20:21 +0300

    Add administrate comments

--- apps/comments/admin.py
+++ apps/comments/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from apps.comments.models import Comment
 
-# Register your models here.
+
+def make_approve(modeladmin, request, queryset):
+    queryset.update(approved=True)
+
+
+make_approve.short_description = 'Одобрить выбранные комментарии'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'article_id',
+        'author',
+        'text',
+        'approved',
+        'created_date'
+    )
+    fields = (
+        'article_id',
+        'author',
+        'email_author',
+        'text',
+        'approved',
+        'created_date'
+    )
+    list_filter = ['created_date']
+    ordering = ('approved', '-created_date',)
+    date_hierarchy = 'created_date'
+    actions = [make_approve]
+
+
+admin.site.register(Comment, CommentAdmin)
