9366b287b939cdde45794a202ad8dbcab097560d
artempy <artemon5@gmail.com>
2017-05-02 18:30:50 +0300

    Alter url-handler for paginate by comments

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^search/$', views.search, name='search'),
     url(r'^(?P<category_slug>[\-\w]+)(/page/(?P<page>[1-9][0-9]?))?/$',
         views.ShowCat.as_view(paginate_by=3), name='category'),
-    url(r'^(?P<category_slug>[\-\w]+)/(?P<article_slug>[\-\w]+)\.html$',
+    url(r'^(?P<category_slug>[\-\w]+)/(?P<article_slug>[\-\w]+)\.html(/comments/page/(?P<page>[1-9][0-9]?))?/?$',
         views.show_article, name='article'),
 ]
