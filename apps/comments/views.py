65350ee679287ab2db0c2d3ef052f22fdec4fc7b
artempy <artemon5@gmail.com>
2017-05-02 18:24:23 +0300

    New functions are show_comments and add_comment

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from apps.comments.models import Comment
+from apps.comments.forms import CommentForm
 
-# Create your views here.
+
+def show_comments(request, article, page):
+    all_comments = Comment.objects.filter(
+        article_id=article
+    ).filter(
+        approved=True
+    ).order_by('-created_date')
+
+    paginator = Paginator(all_comments, 8)
+
+    try:
+        comments = paginator.page(page)
+    except PageNotAnInteger:
+        comments = paginator.page(1)
+    except EmptyPage:
+        comments = paginator.page(paginator.num_pages)
+
+    return comments
+
+
+def add_comment(request, article):
+    """Helper-function for add comment"""
+    success = None
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            success = True
+            comment = form.save(commit=False)
+            # Setting fields forms for save
+            comment.article_id = article
+            comment.approved = False
+            comment.author = form.cleaned_data['author']
+            comment.email_author = form.cleaned_data['email_author']
+            comment.text = form.cleaned_data['text']
+            comment.save()
+            form = CommentForm()
+    else:
+        form = CommentForm()
+
+    return [form, success]
