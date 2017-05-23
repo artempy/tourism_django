abdc4ce4d5e0bc27f2fac72278f2538d3cacc58d
artempy <artemon5@gmail.com>
2017-05-23 23:48:24 +0300

    Add views showing main page. Add view search. Title for show_article was changed

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render_to_response, render, get_object_or_404
+import operator
+from functools import reduce
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.urlresolvers import reverse
 from django.views.generic.list import ListView
+from django.db.models import Q
 from apps.posts.models import Category, Article
 from apps.comments.views import show_comments, add_comment
 
 
 def index(request):
-    return render_to_response('index.html', {})
+    categories = Category.objects.all().order_by('name')
+    for category in categories:
+        # IF p tag occur in text then get first paragraph
+        if '</p>' in category.description:
+            category.description = category.description.split("</p>")[0]\
+                .replace('<p>', '')
+    return render(request, 'index.html', {'categories': categories})
 
 
 def search(request):
-    return render_to_response('index.html', {})
+    if request.method == 'POST':
+        query = request.POST.get('q')
+        if query:
+            query_list = query.split()
+            results = Article.objects.filter(
+                reduce(operator.and_, (Q(name__icontains=q)
+                                       for q in query_list)
+                       )
+            )[:15]
+            return render(request, 'search_results.html',
+                          {'results': results, 'query': query})
+    return redirect(reverse('index'))
 
 
 class ShowCat(ListView):
@@ -50,7 +70,7 @@ def show_article(request, category_slug, article_slug, page=None):
     form, success = add_comment(request, article)
 
     context_dict = {
-        'title_meta': article.title_meta,
+        'title_meta': "%s / %s " % (article.title_meta, cat.name),
         'description_meta': article.description_meta,
         'category': cat,
         'article': article,
