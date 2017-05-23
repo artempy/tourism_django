991ef6d16da26bef5f3291ca1eb64cfb90c38cf7
artempy <artemon5@gmail.com>
2017-05-23 23:59:32 +0300

    Add handling view files in media directory

--- tourism/urls.py
+++ tourism/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,4 +30,5 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    ] + urlpatterns + static(settings.MEDIA_URL,
+                             document_root=settings.MEDIA_ROOT)
