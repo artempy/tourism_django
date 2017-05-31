e5f7fb4731b4c7c402b4c0854e0b1539a133b332
artempy <artemon5@gmail.com>
2017-05-31 20:57:16 +0300

    Add caching

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
 from apps.posts import views
+from django.views.decorators.cache import cache_page
 
+
+FIVE_DAYS = 5 * 24 * 60 * 60
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
     url(r'^(?P<category_slug>[\-\w]+)(/page/(?P<page>[1-9][0-9]?))?/$',
-        views.ShowCat.as_view(paginate_by=3), name='category'),
+        cache_page(FIVE_DAYS)(views.ShowCat.as_view(paginate_by=7)),
+        name='category'),
     url(r'^(?P<category_slug>[\-\w]+)/(?P<article_slug>[\-\w]+)\.html(/comments/page/(?P<page>[1-9][0-9]?))?/?$',
         views.show_article, name='article'),
 ]
