64bf8c6a46d8bd2260a5dd1e8cdf69e6b01261da
artempy <artemon5@gmail.com>
2017-05-31 21:01:36 +0300

    Remove unnecessary funtions

--- tourism/content/js/general.js
+++ tourism/content/js/general.js
@@ -48,22 +48,6 @@ function(){
     };
 });
 
-var loading = false;
-$(window).scroll(function(){
-if((($(window).scrollTop()+$(window).height())+250)>=$(document).height()){
-  if(loading == false){
-  loading = true;
-  $('#loadingbar').css("display","block");
-  $.get("load.php?start="+$('#loaded_max').val(), function(loaded){
-    $('body').append(loaded);
-    $('#loaded_max').val(parseInt($('#loaded_max').val())+50);
-    $('#loadingbar').css("display","none");
-    loading = false;
-  });
-  }
-}
-});
-
 $(document).ready(function() {
     $('#loaded_max').val(50);
     $("i#reply_comment").click(function() {
