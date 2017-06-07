4801470f3ffc939290d4862ec5359c660225a46a
artempy <artemon5@gmail.com>
2017-06-07 20:00:48 +0300

    Add sender ip to determine. Fix field email from

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -16,18 +16,22 @@ def contact_form(request):
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             feedback_email = getattr(settings, 'FEEDBACK_EMAIL')
+            user_ip = request.META['REMOTE_ADDR']
             recipients = [feedback_email]
             # Get current url of site
             site = request.scheme + '://' +\
                 request.META['HTTP_HOST'] + request.path
             date = datetime.now().strftime("%d.%m.%Y в %H:%M")
+            from_email = getattr(settings, 'FROM_EMAIL')
             # Forming message for send
             message = "{1} написал Вам через форму обратной связи с сайта {0}:\
             \n\n{2}\n\nДата: {3}\
-            \nEmail отправителя: {4}".format(site, name, message, date, email)
+            \nEmail отправителя: {4}\
+            \nIP отправителя: {5}".format(site, name, message,
+                                          date, email, user_ip)
 
             try:
-                send_mail(subject, message, email, recipients)
+                send_mail(subject, message, from_email, recipients)
             except BadHeaderError:
                 raise form.ValidationError('Invalid header found!')
             else:
